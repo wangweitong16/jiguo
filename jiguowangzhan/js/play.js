60aa8964c971a8b9d64d77b9edf5d00fcc4f64fd
wangweitong <wangweitong16@outlook.com>
2021-10-02 20:54:42 +0800

    10.2_20:54_play

--- jiguowangzhan/js/play.js
+++ jiguowangzhan/js/play.js
@@ -1,21 +1,25 @@
 var more = document.getElementsByClassName('more')[0];
-more.style.display='none';
-var Ajax = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
-Ajax.open('get', 'http://192.168.1.107:3000/data/play_new.json', true);
-Ajax.send();
+more.style.display = 'none';
 var loading = document.getElementsByClassName('loading')[0];
-Ajax.onreadystatechange = function () {
-    if (Ajax.readyState == 4) {
-        if (Ajax.status == 200) {
-            loading.style.display='none';
-            more.style.display='block';
-            var res = JSON.parse(Ajax.responseText);
-            useres(res);
-        } else {
-            console.log(Ajax.status);
+
+function theAjax(theUrl) {
+    var Ajax = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
+    Ajax.open('get', theUrl, true);
+    Ajax.send();
+    Ajax.onreadystatechange = function () {
+        if (Ajax.readyState == 4) {
+            if (Ajax.status == 200) {
+                loading.style.display = 'none';
+                more.style.display = 'block';
+                var res = JSON.parse(Ajax.responseText);
+                useres(res);
+            } else {
+                console.log(Ajax.status);
+            }
         }
     }
 }
+theAjax('http://192.168.1.107:3000/data/play_new.json');
 
 function useres(res) {
     var html = template('art', {
@@ -28,27 +32,46 @@ function useres(res) {
 
 
 var main = document.getElementsByClassName('main')[0];
-function outhid(){
+
+function outhid() {
     var out = document.getElementsByClassName('out');
-    for(var i = 0;i<out.length;i++){
-        if(i>=16){
+    var m = 1;
+    for (var i = 0; i < out.length; i++) {
+        if (i >= m * 16) {
             out[i].style.display = 'none';
         }
     }
-    more.onclick = function (){
-        if(more.value==1){
-            more.value = 0;
-            for(var i = 0;i<out.length;i++){
-                if(i>=16){
-                    out[i].style.display = 'none';
-                }
+    m++;
+    more.style.display = 'block';
+    if (out.length < m * 16) {
+        more.style.display = 'none';
+    }
+    more.onclick = function () {
+        for (var i = 0; i < out.length; i++) {
+            if (i < 16 * m) {
+                out[i].style.display = 'block';
+            } else {
+                out[i].style.display = 'none';
             }
-        }else{
-            more.value = 1;
-            more.style.display='none';
-            
+        }
+        m++;
+        if (out.length < m * 16) {
+            more.style.display = 'none';
         }
     }
-    
+
 }
 
+// hot 部分 
+
+$('.title>div').on('click',$('div'),function(index){
+    $('.title>div').removeClass('c_color');
+    $(this).addClass('c_color');
+    if($(this).index()==0){
+        theAjax('http://192.168.1.107:3000/data/play_new.json')
+    }else if($(this).index()==1){
+        theAjax('http://192.168.1.107:3000/data/play_hot.json')
+    }else{
+        theAjax('http://192.168.1.107:3000/data/play_category.json')
+    }
+})
